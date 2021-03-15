18cf3ef3df48f1ea3d51d96b16833fb97a57c1a5
eivillamuelas <eivillamuelas@gmail.com>
2021-03-15 23:26:59 +0100

    h

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -26,4 +26,25 @@
     offset: 56
   });
 
+  $(document).ready(function(){
+    
+    // hide .navbar first
+    $(".navbar").hide();
+    
+    // fade in .navbar
+    $(function () {
+      $(window).scroll(function () {
+              // set distance user needs to scroll before we fadeIn navbar
+        if ($(this).scrollTop() > 100) {
+          $('#mainNav').fadeIn();
+        } else {
+          $('#mainNav').fadeOut();
+        }
+      });
+  
+    
+    });
+  
+  });
+
 })(jQuery); // End of use strict
